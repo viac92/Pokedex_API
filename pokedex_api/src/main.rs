536997b04f77f819c2f3c36b245a9f77f0cd046b
viac92 <64146594+viac92@users.noreply.github.com>
2024-06-10 16:29:22 +0200

    Add main.rs to use warp for HTTP server, first iteration.

--- pokedex_api/src/main.rs
+++ pokedex_api/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        // Set the IP address for docker to 0.0.0.0
+        .run(([0, 0, 0, 0], 3030))
+        .await;
 }
